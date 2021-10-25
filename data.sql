e168546afaf7d2d29c549f9e1758cc8e36cf036e
rpire <rubenpire7@gmail.com>
2021-10-25 19:19:00 -0400

    Insert animals data into the animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Agumon', 'Feb 3, 2020', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Gabumon', 'Nov 15, 2018', 2, TRUE, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Devimon', 'May 12, 2017', 5, TRUE, 11);
