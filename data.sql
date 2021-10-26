a0d6737eba9480ac19064997e82e7168e6c16b61
rpire <rubenpire7@gmail.com>
2021-10-26 18:03:25 -0400

    Insert new animals with their respective data

--- data.sql
+++ data.sql
@@ -8,3 +8,16 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
 VAlUES ('Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
 VAlUES ('Devimon', 'May 12, 2017', 5, TRUE, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Charmander', 'Feb 8, 2020', 0, FALSE, -11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Plantmon', 'Nov 15, 2022', 2, TRUE, -5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Squirtle', 'Apr 2, 1993', 3, FALSE, -12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Angemon', 'Jun 12, 2005', 1, TRUE, -45);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Boarmon', 'Jun 7, 2005', 7, TRUE, 20.4);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight)
+VAlUES ('Blossom', 'Oct 13, 1998', 3, TRUE, 17);
