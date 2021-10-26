6f381d06682dc4ed6f99832a40f7babe78541250
rpire <rubenpire7@gmail.com>
2021-10-26 18:04:04 -0400

    Add the new required queries to queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,61 @@ SELECT name, escape_attempts FROM animals WHERE weight > 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+  UPDATE animals
+  SET species = 'Unespecified';
+ROLLBACK;
+
+BEGIN;
+  UPDATE animals
+  SET species = 'Digimon'
+  WHERE name LIKE '%mon';
+
+  UPDATE animals
+  SET species = 'Pokemon'
+  WHERE name NOT LIKE '%mon';
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+  DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+  DELETE FROM animals
+  WHERE date_of_birth > 'Jan 1, 2022';
+  SAVEPOINT sp1;
+    UPDATE animals
+    SET weight = weight * -1;
+  ROLLBACK TO sp1;
+  UPDATE animals
+  SET weight = weight * -1
+  WHERE weight < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts <= 0;
+SELECT AVG(weight) FROM animals;
+
+SELECT
+  neutered, AVG(escape_attempts)
+FROM
+  animals
+GROUP BY neutered;
+
+SELECT
+  species, MIN(weight), MAX(weight)
+FROM
+  animals
+GROUP BY species;
+
+SELECT
+  species, AVG(escape_attempts)
+FROM
+ animals
+WHERE
+  date_of_birth
+BETWEEN 'Jan 1, 1990' AND 'Dec 31, 2000'
+GROUP BY species;
