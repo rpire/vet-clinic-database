015538de1810dc0e7f2824b1a74aecc887cbc689
rpire <rubenpire7@gmail.com>
2021-11-01 12:52:15 -0400

    Insert new required values to visits and owners

--- data.sql
+++ data.sql
@@ -385,3 +385,11 @@ WHERE
   animals.name = 'Blossom'
 AND
   vets.name = 'William Thatcher';
+
+-- This part of the code was repeated 4 times to ensure the queries were taking more than 1s to execute.
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- // --
